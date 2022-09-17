b31be4803a947a18ec13e4085b0928397fc5f2c0
JohnGrib <johngrib82@gmail.com>
2022-09-18 00:03:40 +0900

    3.3 보호된 리소스에 접근하기 위한 토큰 사용

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -84,11 +84,26 @@ app.get('/callback', function(req, res){
 });
 
 app.get('/fetch_resource', function(req, res) {
+    console.log("/fetch_resource 호출");
 
-	/*
-	 * Use the access token to call the resource server
-	 */
-	
+    if (!access_token) {
+        res.render('error', {error: 'Missing access token.'});
+        return;
+    }
+
+    var headers = {
+        'Authorization': 'Bearer ' + access_token
+    };
+    var resource = request('POST', protectedResource, {headers: headers});
+
+    if (resource.statusCode >= 200 && resource.statusCode < 300) {
+        var body = JSON.parse(resource.getBody());
+        res.render('data', {resource: body});
+        return;
+    } else {
+        res.render('error', {error: 'Server returned response code: ' + resource.statusCode});
+        return;
+    }
 });
 
 var buildUrl = function(base, options, hash) {
